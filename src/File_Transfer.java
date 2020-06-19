203150537c35a482e6d1427e8b0679885ac9baed
arshyajabbi <arshyajabbari@gmail.com>
2020-06-19 12:17:33 +0430

    changed file transfer a bit

--- src/File_Transfer.java
+++ src/File_Transfer.java
@@ -1,9 +1,14 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 //this class handles the file transfer faze and search of a file by a peer which finally gets instantiated in TCP Class
 public class File_Transfer {
     private File directory;
     private File[] listOfFiles;
+    private byte[] requestedFile;
+    private FileInputStream fileInputStream;
 
     public File_Transfer(String path){
         this.directory = new File(path);
@@ -19,6 +24,21 @@ public class File_Transfer {
             }
         }
         return false;
+    }
+
+    //returns the wanted file in byte array
+    public byte[] getFile(String name) throws IOException {
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if(file.getName() == name){
+                    fileInputStream= new FileInputStream(file);
+                    fileInputStream.read(requestedFile);
+                    fileInputStream.close();
+                }
+
+            }
+        }
+        return requestedFile;
     }
 
 }
