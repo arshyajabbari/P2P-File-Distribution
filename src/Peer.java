654c79cf5ecf16175eab679f43ec2c8351769fd4
arshyajabbi <arshyajabbari@gmail.com>
2020-07-15 13:14:12 +0430

    editing peer class

--- src/Peer.java
+++ src/Peer.java
@@ -1,66 +1,44 @@
 import java.net.*;
 import java.util.Scanner;
+import java.util.Vector;
 
-public class Peer extends Thread {
+public class Peer implements Runnable {
 
-    private DatagramSocket udpsocket;
-    private InetAddress serverAddress;
-    private Scanner scanner;
+    //private DatagramSocket udpsocket;
+    //private InetAddress serverAddress;
+    //private Scanner scanner;
     private String name;
+    private String IP;
+    private Discovery discovery;
+    //private Vector<String[]> nodeList;
+    private User_Request user_request;
+    private File_Transfer file_transfer;
+    private String directoryPath;
 
     public int tcpport;
     public int udpport;
 
     //first index indicates if is in discovery faze, next one indicates
     //if is in sending file state and the next is for servicing the user
-    private boolean[] state = new boolean[3];
-    private byte[] reading_buffer = new byte[256];
-    private byte[] writing_buffer = new byte[256];
+    //private boolean[] state = new boolean[3];
+    //private byte[] reading_buffer = new byte[256];
+    //private byte[] writing_buffer = new byte[256];
 
     //for initializing the peer
-    public Peer(int udpport,String destination,String name) throws SocketException, UnknownHostException {
+    public Peer(int udpport,int tcpport,String directoryPath,String IP,String name) throws SocketException, UnknownHostException {
         this.udpport=udpport;
-        this.udpsocket = new DatagramSocket(this.udpport);
-        this.serverAddress=InetAddress.getByName(destination);
+        this.tcpport=tcpport;
+        //this.serverAddress=InetAddress.getByName(IP);
+        this.IP=IP;
+        this.directoryPath=directoryPath;
         this.name=name;
+        this.discovery=new Discovery(name,IP);
+        this.user_request=new User_Request();
+        this.file_transfer=new File_Transfer(directoryPath);
     }
 
-    public String getname(){
-        return this.name;
+    @Override
+    public void run() {
+
     }
-
-    //udp server making connection
-    public void udpserver() throws Exception {
-        System.out.println("-- Running Server at " + InetAddress.getLocalHost() + "--");
-        String msg;
-
-        while (true) {
-
-            byte[] buf = new byte[256];
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
-
-            // blocks until a packet is received
-            udpsocket.receive(packet);
-            msg = new String(packet.getData()).trim();
-
-            System.out.println(
-                    "Message from " + packet.getAddress().getHostAddress() + ": " + msg);
-        }
-    }
-
-    //udp client recieving lists
-    public void udpclient() throws Exception {
-        String in;
-        System.out.println("-- Running UDP Client at " + InetAddress.getLocalHost() + " --");
-        while (true) {
-            in = scanner.nextLine();
-
-            DatagramPacket p = new DatagramPacket(
-                    in.getBytes(), in.getBytes().length, serverAddress, udpport);
-
-            this.udpsocket.send(p);
-        }
-    }
-
-
 }
